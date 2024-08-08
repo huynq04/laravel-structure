b7e7791a9ba64da92a8dacec24b07eaab558b609
huynq04 <huynq.b22cn391@stu.ptit.edu.vn>
2024-08-08 14:43:06 +0700

    create structure api

---
+++ app/Mail/VerifyMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user, $url;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($user, $url)
+    {
+        $this->user = $user;
+        $this->url = $url;
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->markdown('mail.verify')->to($this->user->email);
+    }
+}
